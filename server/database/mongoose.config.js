0d7f323164baa24f489c1cabc3e29e6cd1c764a8
Cristian Giraldo <cristian.giraldo@live.com>
2021-10-27 18:47:22 -0500

    bug fix

--- server/database/mongoose.config.js
+++ server/database/mongoose.config.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/encanto-db', (error) => {
     if (error) {
         console.error('Error connecting to mongodb', error);
+    } else {
+        console.log('Successfully connected to mongodb');
     }
-    console.log('Successfully connected to mongodb');
 });
